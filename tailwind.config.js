32aa805f1ef73829948c1d0a4223d8cebec3bb3e
morningsoftware <morningsoftware@proton.me>
2024-10-11 21:19:39 -0400

    Code

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{html,js}", "./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        emerald: {
+          50: "#ecfdf5",
+          100: "#d1fae5",
+          200: "#a7f3d0",
+          300: "#6ee7b7",
+          400: "#34d399",
+          500: "#10b981",
+          600: "#059669",
+          700: "#047857",
+          800: "#065f46",
+          900: "#064e3b",
+          950: "#022c22",
+        },
+      },
+      animation: {
+        flash: "flash 1s infinite",
+      },
+      keyframes: {
+        flash: {
+          "0%, 100%": { opacity: 1 },
+          "50%": { opacity: 0 },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
